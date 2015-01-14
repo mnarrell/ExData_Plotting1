27eb6fc0fe9726dcc052f07623352a7cfb8ba9e2
Matt Narrell <mnarrell@gmail.com>
2015-01-14 10:41:45 -0700

    Rearranges lib calls

--- fetch_data.R
+++ fetch_data.R
@@ -5,8 +5,9 @@
 
 # Fetch/clean the data unless the data frame is in our environment
 if (!exists("power_consumption")) {
-  dataSrcUri <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  # Fetch
   if (!file.exists("./data/household_power_consumption.txt")) {
+    dataSrcUri <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
     temp <- tempfile()
     download.file(dataSrcUri, temp, method = "curl")
     unzip(temp, exdir = "./data")
@@ -14,17 +15,18 @@ if (!exists("power_consumption")) {
     rm(temp)
   }
   
-  library("sqldf")
-  library("dplyr")
-  library("lubridate")
   # Only read the data for the dates we're interested in
+  library("dplyr")
+  library("sqldf")  
   power_consumption <- tbl_df(read.csv.sql(file = "./data/household_power_consumption.txt", 
                                            sql = "SELECT * FROM file WHERE Date = '1/2/2007' OR Date = '2/2/2007'", 
                                            stringsAsFactors = FALSE, header = TRUE, sep = ";"))
   
-  power_consumption <- power_consumption %>% 
-    # Build a Timestamp variable from the Date and Time variables
+  # Date time
+  # Build a Timestamp variable from the Date and Time variables
+  # Drop the Date and Time variables in favor of the Timestamp
+  library("lubridate")
+  power_consumption <- power_consumption %>%     
     mutate(Timestamp = dmy_hms(paste(power_consumption$Date, power_consumption$Time))) %>% 
-    # Drop the Date and Time variables in favor of the Timestamp
     select(Timestamp, (Global_active_power:Sub_metering_3))
 } 
--- plot3.R
+++ plot3.R
@@ -14,5 +14,5 @@ plot(power_consumption$Timestamp, power_consumption$Sub_metering_1, type = "l", 
 lines(power_consumption$Timestamp, power_consumption$Sub_metering_2, type = "l", col = "red")
 lines(power_consumption$Timestamp, power_consumption$Sub_metering_3, type = "l", col = "blue")
 # Draw the legend
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 2, col = c("black", "red", "blue"))
+legend("topright",lwd = 2, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"))
 dev.off() 
